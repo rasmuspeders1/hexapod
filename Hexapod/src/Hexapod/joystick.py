aeb86afdd06991c227183497b22e0fcec103b9e3
Rasmus Pedersen <rasmuspeders1@gmail.com>
2011-09-17 14:33:10 +0200

    Added empty joystick input class

--- Hexapod/src/Hexapod/joystick.py
+++ Hexapod/src/Hexapod/joystick.py
@@ -3,3 +3,11 @@ Created on Sep 10, 2011
 
 @author: rasmus
 '''
+
+import pygame
+
+class RobotInput:
+    '''
+    Class to handle joystick input and translate it to something more useful 
+    for robot control.
+    '''
